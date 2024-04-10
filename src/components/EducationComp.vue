1283db51d4ba283bb24d3db8c189b3c349730e4f
Brandon <brandonroulstone71@gmail.com>
2024-04-10 08:40:53 +0200

    new commit

--- src/components/EducationComp.vue
+++ src/components/EducationComp.vue
@@ -3,15 +3,6 @@
         <div class="container px-4 py-5 gap-3" id="hanging-icons">
             <h2 class="pb-2 display-4 text-black pt-5">Education History</h2>
             <div class="row g-4 py-5 row-cols-1 row-cols-lg-1">
-                <div class="col d-flex align-items-start justify-content-center py-5 px-3" id="bgCard1">
-                    <div class="icon-square bg-light px-3 py-3 rounded-5 text-dark flex-shrink-0 me-3 border border-black">
-                        <i class="fa-solid fa-school fa-lg" style="color: #000000;"></i>
-                    </div>
-                    <div id="bcgsm" class="py-4 px-4 shadow">
-                        <h2>Heathfield High School</h2>
-                        <p>Matriculated in 2021</p>
-                    </div>
-                </div>
 
                 <div class="col d-flex align-items-start justify-content-center py-5 px-3" id="bgCard2">
                     <div class="icon-square bg-light px-3 py-3 rounded-5 shadow text-dark flex-shrink-0 me-3 border border-black">
@@ -20,6 +11,16 @@
                     <div id="bcgsm" class="py-4 px-4 shadow">
                         <h2>Life Choices Academy</h2>
                         <p>Graduated in 2024 - Cohort-13</p>
+                    </div>
+                </div>
+
+                <div class="col d-flex align-items-start justify-content-center py-5 px-3" id="bgCard1">
+                    <div class="icon-square bg-light px-3 py-3 rounded-5 text-dark flex-shrink-0 me-3 border border-black">
+                        <i class="fa-solid fa-school fa-lg" style="color: #000000;"></i>
+                    </div>
+                    <div id="bcgsm" class="py-4 px-4 shadow">
+                        <h2>Heathfield High School</h2>
+                        <p>Matriculated in 2021</p>
                     </div>
                 </div>
             </div>
@@ -31,16 +32,7 @@
         <div class="container px-4 py-5 gap-3" id="hanging-icons">
             <h2 class="pb-2 display-4 text-black">Work History</h2>
             <div class="row g-4 py-5 row-cols-1 row-cols-lg-1">
-                <div class="col d-flex align-items-start justify-content-center py-5 px-3" id="bgCard1">
-                    <div class="icon-square bg-light px-3 py-3 rounded-5 text-dark flex-shrink-0 me-3 border border-black">
-                        <i class="fa-solid fa-code fa-lg" style="color: #000000;"></i>
-                    </div>
-                    <div id="bcgsm" class="py-4 px-4 shadow">
-                        <h2>Freelancer</h2>
-                        <p>I built websites using Vue Js and React Js and using Node Js as my backend for client websites, I am proficient in these two frontend end frameworks along with Nuxt Js, which I picked up on my way in my freelancing journey, I gained soft skills like nogotiating,team work and communication from this work journey</p>
-                    </div>
-                </div>
-    
+
                 <div class="col d-flex align-items-start justify-content-center py-5 px-3" id="bgCard2">
                     <div class="icon-square bg-light px-3 py-3 rounded-5 shadow text-dark flex-shrink-0 me-3 border border-black">
                         <i class="fa-solid fa-code fa-lg" style="color: #000000;"></i>
@@ -48,6 +40,16 @@
                     <div id="bcgsm" class="py-4 px-4 shadow">
                         <h2>Life Choices Studio</h2>
                         <p>During my time as an intern in LCS as a web Developer I got the chance to work in a team and I obtained soft skills like team work,and comminication skills which enabled me to be more resilient, and utilized my time managment effectively.At the time at lcs I got to learn new technologies like C++ and python and use it in real time and made me me more proficient in the development sector</p>
+                    </div>
+                </div>
+
+                <div class="col d-flex align-items-start justify-content-center py-5 px-3" id="bgCard1">
+                    <div class="icon-square bg-light px-3 py-3 rounded-5 text-dark flex-shrink-0 me-3 border border-black">
+                        <i class="fa-solid fa-code fa-lg" style="color: #000000;"></i>
+                    </div>
+                    <div id="bcgsm" class="py-4 px-4 shadow">
+                        <h2>Freelancer</h2>
+                        <p>I built websites using Vue Js and React Js and using Node Js as my backend for client websites, I am proficient in these two frontend end frameworks along with Nuxt Js, which I picked up on my way in my freelancing journey, I gained soft skills like nogotiating,team work and communication from this work journey</p>
                     </div>
                 </div>
             </div>
